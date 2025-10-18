807d2d8ea84a1317ab638ef7868922b88ab9e5c4
Nekodev <97458908+NotNekodev@users.noreply.github.com>
2025-10-18 19:29:40 +0200

    Replace title screen ImGui demo with an linking button to the example screen (#26)

--- src/main/java/de/florianmichael/imguiexample/mixin/TitleScreenMixin.java
+++ src/main/java/de/florianmichael/imguiexample/mixin/TitleScreenMixin.java
@@ -1,22 +1,34 @@
 package de.florianmichael.imguiexample.mixin;
 
 import de.florianmichael.imguiexample.imgui.RenderInterface;
+import de.florianmichael.imguiexample.screens.ExampleScreen;
 import imgui.ImGui;
 import imgui.ImGuiIO;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.TitleScreen;
+import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.text.Text;
+import net.minecraft.world.gen.GeneratorOptions;
+import net.minecraft.world.gen.WorldPresets;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TitleScreen.class)
-public class TitleScreenMixin implements RenderInterface {
+public class TitleScreenMixin extends Screen {
 
-    @Override
-    public void render(ImGuiIO io) {
-        ImGui.begin("Hello World");
-        // Draw something here, see the official example module for more information:
-        // https://github.com/ocornut/imgui/blob/master/imgui_demo.cpp
-        ImGui.end();
-
-        ImGui.showDemoWindow();
+    protected TitleScreenMixin(Text title) {
+        super(title);
     }
 
+    @Inject(method = "init", at = @At("RETURN"))
+    public void render(CallbackInfo ci) {
+        this.addDrawableChild(ButtonWidget.builder(Text.literal("ImGui Demo"), (button) -> {
+            MinecraftClient.getInstance().setScreen(new ExampleScreen());
+        }).dimensions(this.width - 75 - 3, 3, 70, 20).build());
+    }
 }
